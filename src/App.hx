c4a38212ce9bac60ce37e9d89e8f53073c52b24b
Matthijs Kamstra <matthijs@fonk.amsterdam>
2017-06-18 09:43:57 +0200

    wrap text with bold (**) and italic (_)

--- src/App.hx
+++ src/App.hx
@@ -26,7 +26,7 @@ class App {
     var inMarkdown : DivElement;
 	var outMarkdown : DivElement;
 
-	var markdowExample1 : String = '# heading 1\n\ntest';
+	var markdowExample1 : String = '# heading 1\n\nthis is a test piece of text just for testing\n\n## heading 2\n\njust because we can\n\n- one\n- two';
 	var markdowExample2 : String = haxe.Resource.getString("markdown00");
 
     public function new()
@@ -44,8 +44,10 @@ class App {
 			inMarkdown.oninput = onChange;
 			// inMarkdown.onchange = onChange;
 
-			inMarkdown.innerText = (markdowExample2);
-			convert(markdowExample2);
+			var md = markdowExample1;
+
+			inMarkdown.innerText = (md);
+			convert(md);
 
 			// register the handler
 			document.getElementById('file-upload').addEventListener('change', readSingleFile, false);
@@ -73,14 +75,20 @@ class App {
 		// CMD
 		if(e.metaKey){
 			switch(e.key.toLowerCase()){
-				case '1': trace('change to h1'); keyboardShortCut('1');
+				case '1':
+					trace('change to h1');
+					keyboardShortCut('1');
 				case '2': trace('change to h2'); keyboardShortCut('2');
 				case '3': trace('change to h3'); keyboardShortCut('3');
 				case '4': trace('change to h4'); keyboardShortCut('4');
 				case '5': trace('change to h5'); keyboardShortCut('5');
 				case '6': trace('change to h6'); keyboardShortCut('6');
-				case 'b': trace('change to bold');  keyboardShortCut('b');
-				case 'i': trace('change to italic');
+				case 'b':
+					trace('change to bold');
+					wrap('**');
+				case 'i':
+					trace('change to italic');
+					wrap('_');
 				case 's': trace('save');
 				case 'o': trace('open');
 			}
@@ -97,48 +105,95 @@ class App {
 
 	function keyboardShortCut(cmdKey:String){
 		// trace('vv '+getCaretPosition(inMarkdown));
-		replaceSelectedText('**');
+		// replaceSelectedText('**');
+		wrap('**');
 	}
 
-	function replaceSelectedText(replacementText:String) {
-		var sel : Selection;
-		var range : Range;
+	function wrap(tag:String) {
+		var sel, range;
+		var selectedText:String;
+
 		if (window.getSelection != null) {
 			sel = window.getSelection();
 			if (sel.rangeCount != null) {
 				range = sel.getRangeAt(0);
+				selectedText = Std.string(range);
 				range.deleteContents();
-				range.insertNode(document.createTextNode(replacementText + sel + replacementText));
+				range.insertNode(document.createTextNode(tag + selectedText + tag));
 			}
-		} else if (untyped document.selection && document.selection.createRange) {
-			range = untyped document.selection.createRange();
-			untyped range.text = replacementText;
 		}
+		// else if (document.selection && document.selection.createRange) {
+		// 	range = document.selection.createRange();
+		// 	selectedText = document.selection.createRange().text + "";
+		// 	range.text = '[' + tag + ']' + selectedText + '[/' + tag + ']';
+		// }
+
 	}
 
 
-	function getCaretPosition(editableDiv) {
-		var caretPos = 0;
-		var range;
-		var selection : Selection;
-		if (window.getSelection != null) {
-			selection = window.getSelection();
-			trace( 'selection: ' + selection );
-			trace(selection.anchorNode );
-			trace(selection.anchorOffset );
-			trace(selection.caretBidiLevel );
-			trace(selection.containsNode );
-			trace(selection.focusNode );
-			trace(selection.rangeCount );
-			if (selection.rangeCount != null) {
-				range = selection.getRangeAt(0);
-				if (range.commonAncestorContainer.parentNode == editableDiv) {
-					caretPos = range.endOffset;
-				}
-			}
-		}
-		return caretPos;
-	}
+	// function surroundSelection(wrapperText:String) {
+	// 	if (window.getSelection != null) {
+	// 		var sel = window.getSelection();
+	// 		if (sel.rangeCount != null) {
+	// 			var range = sel.getRangeAt(0).cloneRange();
+	// 			range.surroundContents(new Node(wrapperText));
+	// 			sel.removeAllRanges();
+	// 			sel.addRange(range);
+	// 		}
+	// 	}
+	// }
+
+
+	// function replaceSelectedText(replacementText:String) {
+	// 	var sel : Selection;
+	// 	var range : Range;
+	// 	var range2 : Range;
+	// 	if (window.getSelection != null) {
+	// 		sel = window.getSelection();
+
+	// 		trace(sel);
+
+	// 		if (sel.rangeCount != null) {
+	// 			range = sel.getRangeAt(0);
+	// 			range2 = sel.getRangeAt(sel.focusOffset - sel.anchorOffset);
+
+	// 			// range.deleteContents();
+
+
+	// 			range.insertNode(document.createTextNode(replacementText));
+	// 			range2.insertNode(document.createTextNode(replacementText));
+
+
+	// 		}
+	// 	} else if (untyped document.selection && document.selection.createRange) {
+	// 		range = untyped document.selection.createRange();
+	// 		untyped range.text = replacementText;
+	// 	}
+	// }
+
+
+	// function getCaretPosition(editableDiv) {
+	// 	var caretPos = 0;
+	// 	var range;
+	// 	var selection : Selection;
+	// 	if (window.getSelection != null) {
+	// 		selection = window.getSelection();
+	// 		trace( 'selection: ' + selection );
+	// 		trace(selection.anchorNode );
+	// 		trace(selection.anchorOffset );
+	// 		trace(selection.caretBidiLevel );
+	// 		trace(selection.containsNode );
+	// 		trace(selection.focusNode );
+	// 		trace(selection.rangeCount );
+	// 		if (selection.rangeCount != null) {
+	// 			range = selection.getRangeAt(0);
+	// 			if (range.commonAncestorContainer.parentNode == editableDiv) {
+	// 				caretPos = range.endOffset;
+	// 			}
+	// 		}
+	// 	}
+	// 	return caretPos;
+	// }
 
 	function readSingleFile(e) {
 		// trace(e);
